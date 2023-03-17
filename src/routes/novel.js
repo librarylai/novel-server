a1f161b9b6e260a8ac761ebfaa78d122a745d898
librarylai <library0515@gmail.com>
2023-03-18 00:33:23 +0800

    feat: update project structure

--- src/routes/novel.js
+++ src/routes/novel.js
@@ -2,6 +2,6 @@ import { puppeteerNovel } from '../utils/puppeteer'
 
 router.get('/', async (req, res, next) => {
   const param = req.body.url
-  const res = await puppeteerNovel('https://t.uukanshu.com/read.aspx?tid=65713&sid=10962')
-  console.log('res', res)
+  const result = await puppeteerNovel('https://t.uukanshu.com/read.aspx?tid=65713&sid=10962')
+  console.log('result', result)
 })
