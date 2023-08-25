0a3871e7f910a132fee079fa209b198b79ed85a0
librarylai <library0515@gmail.com>
2023-08-26 02:46:22 +0800

    feat: use waitUntil

--- src/utils/puppeteer/nike.js
+++ src/utils/puppeteer/nike.js
@@ -9,29 +9,26 @@ export const puppeteerNikeLogin = async (url = NIKE_LOGIN_URL) => {
 
   // 開啟分頁並前往 Nike 列表
   const page = await browser.newPage()
-  await page.goto(url)
-  //  等待 2 秒讓網站 render 完成
-  setTimeout(async () => {
-    // 取得 html
-    let html = await page.content()
-    // 爬取內容
-    const joinUs = await page.$('.nike-unite-component.action-link.current-member-signin a')
+  await page.goto(url, { waitUntil: 'networkidle2' }) // networkidle2: avigation is finished when there are no more than 2 network connections for at least 500 ms.
+  // 取得 html
+  let html = await page.content()
+  // 爬取內容
+  const joinUs = await page.$('.nike-unite-component.action-link.current-member-signin a')
 
-    if (joinUs) {
-      await joinUs.click()
-      await page.type('input[name="emailAddress"]', 'aaaaa444@gmail.com', { delay: 100 })
-      await page.type('input[name="password"]', 'KI0000123la', { delay: 100 })
-      await page.type('input[name="lastName"]', 'ADR', { delay: 100 })
-      await page.type('input[name="firstName"]', 'WEN', { delay: 100 })
-      await page.$eval('input[name="dateOfBirth"]', (e) => (e.value = '1990-01-01'))
-      await page.select('select[name="country"]', 'TW')
-      await page.type('input[name="gender"]', 'M', { delay: 100 }) // M or F
-      await page.click('ul[data-componentname="gender"] input[type=button]')
-      const submitBtn = await page.$('input[value="加入"]')
-      await submitBtn.click()
-    } else {
-    }
-    await browser.close()
-    return null
-  }, 2000)
+  if (joinUs) {
+    await joinUs.click()
+    await page.type('input[name="emailAddress"]', 'aaaaa444@gmail.com', { delay: 100 })
+    await page.type('input[name="password"]', 'KI0000123la', { delay: 100 })
+    await page.type('input[name="lastName"]', 'ADR', { delay: 100 })
+    await page.type('input[name="firstName"]', 'WEN', { delay: 100 })
+    await page.$eval('input[name="dateOfBirth"]', (e) => (e.value = '1990-01-01'))
+    await page.select('select[name="country"]', 'TW')
+    await page.type('input[name="gender"]', 'M', { delay: 100 }) // M or F
+    await page.click('ul[data-componentname="gender"] input[type=button]')
+    const submitBtn = await page.$('input[value="加入"]')
+    await submitBtn.click()
+  } else {
+  }
+  await browser.close()
+  return null
 }
